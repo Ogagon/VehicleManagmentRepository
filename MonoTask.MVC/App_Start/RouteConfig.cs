dcc700aa0cf7ed6eeb7ac02f68b6cb1068d1a470
Goran Trifunović <goran.trifunovic@outlook.com>
2025-11-28 16:44:32 +0100

    Changed the startup route to point to VehicleMakeController.

--- MonoTask.MVC/App_Start/RouteConfig.cs
+++ MonoTask.MVC/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace MonoTask.MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "VehicleModel", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "VehicleMake", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
