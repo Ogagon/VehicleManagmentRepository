45e55cfc28572a749a7cc2309b67fbfdb0b427fd
Goran Trifunović <goran.trifunovic@outlook.com>
2025-11-29 16:10:22 +0100

    Added index view models for vehiclemake and vehiclemodel

---
+++ MonoTask.MVC/ViewModels/VehicleMake/VehicleMakeIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MonoTask.Service.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonoTask.MVC.ViewModels.VehicleMake
+{
+    public class VehicleMakeIndexViewModel
+    {
+        public PagingResult<VehicleMakeViewModel> PagingResult { get; set; }
+        public VehicleQuery Query { get; set; }
+        public SelectList SelectMakes { get; set; }
+    }
+}
