df9ea7ff8b2fae70e4a2f61ace04ff9dbd0acaee
Goran Trifunović <goran.trifunovic@outlook.com>
2025-11-29 15:09:00 +0100

    Added a DTO for VehicleQuery which will contain query parameters for searching, filtering, and sorting vehicle makes and models. Added folders inside of viewmodels for vehicle make, and vehicle model view models.

---
+++ MonoTask.Service/DTO/VehicleQuery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoTask.Service.DTO
+{
+    public class VehicleQuery
+    {
+        public string CurrentSortColumn { get; set; } = "Name";
+        public bool CurrentSortDescending { get; set; } = false;
+        public string CurrentSearchTerm { get; set; }
+        public int? CurrentMakeId { get; set; }
+        public static readonly Dictionary<string, string> allowedSortingColumns = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "Name", "Name" },
+                { "Abrv", "Abrv" },
+                { "Make", "Make.Name" }
+            };
+        public VehicleQuery(string currentSortColumn, bool currentSortDescending, string currentSearchTerm, int? currentMakeId)
+        {
+            if (!allowedSortingColumns.TryGetValue(currentSortColumn, out var sortColumn))
+            {
+                sortColumn = "Name";
+            }
+            CurrentSortColumn = sortColumn;
+            CurrentSortDescending = currentSortDescending;
+            CurrentSearchTerm = currentSearchTerm;
+            CurrentMakeId = currentMakeId;
+        }
+    }
+}
