129ee31ce1148b82ea066d7f6d4e514413e85960
Goran Trifunović <goran.trifunovic@outlook.com>
2025-12-04 22:58:29 +0100

    - Added custom filters for exceptions

---
+++ MonoTask.MVC/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonoTask.MVC.Filters
+{
+    public class CustomExceptionFilter : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            var errorMessage = "An error has occured. The following error has interrupted the execution of the application: " + filterContext.Exception.Message;
+            filterContext.Result = new ViewResult
+            {
+                ViewName = "~/Views/Shared/Error.cshtml",
+                ViewData = new ViewDataDictionary
+                {
+                    {"Error", errorMessage}
+                }
+            };
+            filterContext.ExceptionHandled = true;
+        }
+    }
+}
--- MonoTask.MVC/Global.asax.cs
+++ MonoTask.MVC/Global.asax.cs
@@ -1,3 +1,4 @@
+using MonoTask.MVC.Filters;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -8,8 +9,9 @@ namespace MonoTask.MVC
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
             GlobalFilters.Filters.Add(new HandleErrorAttribute());
+            GlobalFilters.Filters.Add(new CustomExceptionFilter());
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
